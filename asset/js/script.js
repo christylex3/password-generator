ef93a5a86625b2baa61fb4329078b091a2de97d8
Christy Le <christylex3@gmail.com>
2022-06-24 16:07:28 -0400

    Added comments to code to help user better under the code

--- asset/js/script.js
+++ asset/js/script.js
@@ -11,25 +11,30 @@ var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbers = "01234567890";
 var specialChar = "~`!@#$%^&*()-_+={}[]|\\/:;\"'<>,.?";
 
+// Method prompts the user to get their preferences
 function userPreferences () {
   userCharAnswer = prompt("How many characters should the password contain? (8 - 128)");
 
+  // If user's input is not a number. alert user
   if(isNaN(userCharAnswer)) {
     alert("Invalid answer. Please enter a number.");
     return;
   }
   
+  // If user's input is not a number between 8 and 128, alert user
   if (userCharAnswer < 8 || userCharAnswer > 128) {
     alert("Invalid answer. Please enter a number between 8 and 128.");
     return;
   }
 
+  // Asks the user if they want their generated password to contain certain character types
   userLowercaseAnswer = window.confirm("Should the password contain lowercase letters?");
   userUppercaseAnswer = window.confirm("Should the password contain uppercase letters?");
   userNumberAnswer = window.confirm("Should the password contain numbers?");
   userSpecialCharAnswer = window.confirm("Should the password contain special characters?");
 }
 
+// Method generates the password based upon the user's preferences
 function generatePassword() {
   userPreferences();
 
@@ -52,10 +57,12 @@ function generatePassword() {
     generator += specialChar;
   }
 
+  // If user does not select any criteria, then alert user. Also, if user's input is invalid for "how many char" prompt, do not show alert
   if (generator === "" && (userCharAnswer > 7 && userCharAnswer < 129)) {
     alert("Please select at least one criteria.");
   }
 
+  // Generating user's random password based upon user's char answer and other char types preferences
   for (var i = 0; i < userCharAnswer; i++) {
     result += generator.charAt(Math.floor(Math.random() * (generator.length-1)));
   }
