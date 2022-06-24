a76980b055d71b52207ea59bfb80f3abf98a5dbd
Christy Le <christylex3@gmail.com>
2022-06-24 02:08:13 -0400

    Creating generator based upon user's input

--- asset/js/script.js
+++ asset/js/script.js
@@ -11,11 +11,78 @@ var generateBtn = document.querySelector("#generate");
   // Step #3: Generate password based on criteria.
   // Step #4: Display password to the page. (Already done)
 
+// *** charCodeAt() returns the unicode of the character at a specificed positon in a string
+
+// Testing unicode process
+// var alphabet = "abcdefghijklmnopqrstuvwxyz";
+// var specialChar = "!";
+// var getUnicode = alphabet.charCodeAt(specialChar);
+
+// console.log(getUnicode);
+
+// *** Shows whose unicode it is
+// *** String.fromCharCode();
+// *** 97 = a
+// *** 122 = z
+// *** 65 = uppercase
+
+// lowercaseArray = [
+//   a,
+//   b,
+//   c,
+//   d,
+//   e,
+//   f,
+//   g,
+//   h,
+//   i
+// ]
+
+var codeRevealed = String.fromCharCode();
+console.log(codeRevealed);
+
+//for lowercase
+// var numCode;
+for (var n = 97; n > 123; n++) {
+  var numCode = String.fromCharCode(n);
+  console.log(numCode);
+}
+
+var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz"
+var uppercaseLetters = lowercaseLetters.toUpperCase();
+var numbers = "01234567890";
+// var specialCharacters = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+// console.log(specialCharacters);
+
+// console.log(lowercaseLetters);
+// console.log(uppercaseLetters);
+
+
+// **** Create for-loop to store all lowercase letters, uppercase letters, special characters, and numbers from unicode
+// var hi = "hi"
+// var goodbye = "bye"
+// console.log(hi += bye);
+
+// var uppercaseArray = 
+// var userPrefSaved = [userLowercaseAnswer, userUppercaseAnswer, ];
+
   function userPreferences (x) {
+    var generator = "";
+    var result = "";
     var userLowercaseAnswer = window.confirm("Should the password contain lowercase letters?");
     var userUppercaseAnswer = window.confirm("Should the password contain uppercase letters?");
     var userNumberAnswer = window.confirm("Should the password contain numbers?");
     var userSpecialNumAnswer = window.confirm("Should the password contain special characters?");
+    if (userLowercaseAnswer) {
+      generator += lowercaseLetters;
+    }
+    if (userUppercaseAnswer) {
+      generator += uppercaseLetters;
+    }
+    if (userNumberAnswer) {
+      generator += numbers;
+    }
+    var result 
   }
 
 
@@ -23,18 +90,12 @@ var generateBtn = document.querySelector("#generate");
 // console.log(testCase);
 
 function generatePassword() {
-
-// Prompts user to put in a number between 8 and 128
   var userCharAnswer = prompt("How many characters should the password contain? (8 - 128)");
-
-  // Tests if the user's input is a number
   if (isNaN(userCharAnswer)) {
     alert("Invalid answer. Please enter a number.");
   } else {
-    // Checks if the user's input is in range of 8 and 128
     if (userCharAnswer > 7 && userCharAnswer < 129) {
       userPreferences(userCharAnswer);
-      
       // return*******
     } else {
       alert("Invalid answer. Please enter a number between 8 and 128.");
