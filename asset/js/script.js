67c40150c771387859cd590a01a935242a1316b1
Christy Le <christylex3@gmail.com>
2022-06-24 09:46:11 -0400

    Linked generatePassword(), userPreferences(), and writePassword() together. Now working on generating random password based upon preferences

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,8 +1,5 @@
 // Assignment Code
-
-// it's the red button
 var generateBtn = document.querySelector("#generate");
-
 
   // Step #1: Prompt the user for the password criteria:
   //      a) Password length: minimum 8 characters and maximum 128 characters
@@ -12,53 +9,6 @@ var generateBtn = document.querySelector("#generate");
   // Step #4: Display password to the page. (Already done)
 
 // *** charCodeAt() returns the unicode of the character at a specificed positon in a string
-
-// Testing unicode process
-// var alphabet = "abcdefghijklmnopqrstuvwxyz";
-// var specialChar = "!";
-// var getUnicode = alphabet.charCodeAt(specialChar);
-
-// console.log(getUnicode);
-
-// *** Shows whose unicode it is
-// *** String.fromCharCode();
-// *** 97 = a
-// *** 122 = z
-// *** 65 = uppercase
-
-// lowercaseArray = [
-//   a,
-//   b,
-//   c,
-//   d,
-//   e,
-//   f,
-//   g,
-//   h,
-//   i
-// ]
-
-var codeRevealed = String.fromCharCode();
-console.log(codeRevealed);
-
-//for lowercase
-// var numCode;
-for (var n = 97; n > 123; n++) {
-  var numCode = String.fromCharCode(n);
-  console.log(numCode);
-}
-
-// console.log(lowercaseLetters);
-// console.log(uppercaseLetters);
-
-
-// **** Create for-loop to store all lowercase letters, uppercase letters, special characters, and numbers from unicode
-// var hi = "hi"
-// var goodbye = "bye"
-// console.log(hi += bye);
-
-// var uppercaseArray = 
-// var userPrefSaved = [userLowercaseAnswer, userUppercaseAnswer, ];
 
 var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz"
 var uppercaseLetters = lowercaseLetters.toUpperCase();
@@ -85,12 +35,10 @@ var specialChar = "~`!@#$%^&*()-_+={}[]|\\/:;\"'<>,.?";
     if (userSpecialNumAnswer) {
       generator += specialChar
     }
-    result = Math.random() * generator
+    return generator;
+    // result = Math.random() * generator
   }
 
-
-// var testCase = window.confirm("Hi");
-// console.log(testCase);
 
 function generatePassword() {
   var userCharAnswer = prompt("How many characters should the password contain? (8 - 128)");
@@ -98,16 +46,12 @@ function generatePassword() {
     alert("Invalid answer. Please enter a number.");
   } else {
     if (userCharAnswer > 7 && userCharAnswer < 129) {
-      userPreferences(userCharAnswer);
-      // return*******
+      return userPreferences(userCharAnswer);
     } else {
       alert("Invalid answer. Please enter a number between 8 and 128.");
     }
   }
 }
-
-  //needs to return the generated password. So make genrated password a variable and return the variable
-  // return;
 
 // Write password to the #password input
 function writePassword() {
